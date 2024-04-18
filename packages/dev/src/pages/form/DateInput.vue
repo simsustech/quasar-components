437e3b5a00c0ac7cf3bb30a971d2bc17bd86857c
Stefan van Herwijnen <stefan@devliert.info>
2024-04-18 12:25:57 +0200

    chore: add DateInput dev page

--- packages/dev/src/pages/form/DateInput.vue
+++ packages/dev/src/pages/form/DateInput.vue
@@ -1,12 +1,14 @@
 <template>
   {{ date }}
-  <date-input v-model="date" style="max-width: 200px" />
+  <date-input v-model="date" style="max-width: 250px" />
   <date-input
+    ref="dateInputRef"
     v-model="date"
     clearable
     format="DD-MM-YYYY"
     locale="nl"
-    style="max-width: 200px"
+    style="max-width: 250px"
+    required
   />
 </template>
 
@@ -14,5 +16,6 @@
 import { DateInput } from "@simsustech/quasar-components/form";
 import { ref } from "vue";
 
-const date = ref(null);
+const date = ref("");
+const dateInputRef = ref();
 </script>
